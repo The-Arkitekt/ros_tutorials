f04e20dc511d5c36a7b39924a73e24e07ec7c4f1
The-Arkitekt <mr.davidcrumley@gmail.com>
2022-08-30 12:16:30 -0400

    Removed all build and devel files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davie_jones/ros_workspaces/ros_tutorials/src
-# Build directory: /home/davie_jones/ros_workspaces/ros_tutorials/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("tutorial_one")
--- build/tutorial_one/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davie_jones/ros_workspaces/ros_tutorials/src/tutorial_one
-# Build directory: /home/davie_jones/ros_workspaces/ros_tutorials/build/tutorial_one
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
